b131b70e054b171eb831f952965a069d2633f78e
ganesh-sherkar <sherkarg527@gmail.com>
2026-01-11 15:30:55 +0530

    add

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -10,65 +10,33 @@ const calculateTotal = (items) => {
 
 // ➕ ADD TO CART
 exports.addToCart = async (req, res) => {
-  try {
-    const { userId, product } = req.body;
+  const { userId, product } = req.body;
 
-    // 🔒 VALIDATION (THIS WAS MISSING)
-    if (!userId || !product || !product.productId) {
-      return res.status(400).json({
-        message: "Invalid cart data"
-      });
-    }
+  let cart = await Cart.findOne({ userId });
 
-    let cart = await Cart.findOne({ userId });
-
-    if (!cart) {
-      cart = await Cart.create({
-        userId,
-        items: [{
-          productId: product.productId,
-          name: product.name,
-          price: product.price,
-          quantity: product.quantity || 1,
-          image: product.image
-        }],
-        totalPrice: product.price * (product.quantity || 1)
-      });
-    } else {
-      const index = cart.items.findIndex(
-        i => i.productId.toString() === product.productId
-      );
-
-      if (index > -1) {
-        cart.items[index].quantity += product.quantity || 1;
-      } else {
-        cart.items.push({
-          productId: product.productId,
-          name: product.name,
-          price: product.price,
-          quantity: product.quantity || 1,
-          image: product.image
-        });
-      }
-
-      cart.totalPrice = cart.items.reduce(
-        (sum, i) => sum + i.price * i.quantity,
-        0
-      );
-
-      await cart.save();
-    }
-
-    res.json(cart);
-
-  } catch (error) {
-    console.error("ADD TO CART ERROR:", error);
-    res.status(500).json({
-      message: "Add to cart failed"
+  if (!cart) {
+    cart = await Cart.create({
+      userId,
+      items: [product],
+      totalPrice: product.price * product.quantity
     });
-  }
-};
+  } else {
+    const index = cart.items.findIndex(
+      (i) => i.productId.toString() === product.productId
+    );
 
+    if (index > -1) {
+      cart.items[index].quantity += product.quantity;
+    } else {
+      cart.items.push(product);
+    }
+
+    cart.totalPrice = calculateTotal(cart.items);
+    await cart.save();
+  }
+
+  res.json(cart);
+};
 
 // 📥 GET CART
 exports.getCart = async (req, res) => {
