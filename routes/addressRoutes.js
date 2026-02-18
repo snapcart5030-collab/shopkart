45462af747a913503f84e1666acc54088eb967e4
ganesh-sherkar <sherkarg527@gmail.com>
2026-02-18 23:30:41 +0530

    address

--- routes/addressRoutes.js
+++ routes/addressRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const protect = require("../middlewares/authMiddleware");
-const { checkDelivery } = require("../controllers/addressController");
 
 const {
   addAddress,
@@ -14,6 +13,5 @@ router.post("/", protect, addAddress);
 router.get("/", protect, getAddresses);
 router.put("/:id", protect, updateAddress);
 router.delete("/:id", protect, deleteAddress);
-router.post("/check-delivery", protect, checkDelivery);
 
 module.exports = router;
